f13b4f61821f32c7e4037ad2fb6a83dd20ad43b5
buvanamanibabu <buvanamanibabu@outlook.com>
2020-08-16 02:40:25 -0700

    Added stored procedures for history tables

---
+++ EverGreenDw/StoredProcedure/dbo.Populate_Membership_Renewal_History.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROC [dbo].[Populate_Membership_Renewal_History]
+AS
+BEGIN
+	WITH distinctdate AS
+	(
+	select distinct contact_id,start_date as Start_Date,end_date as End_Date
+	from dbo.stage_civicrm_membership
+	where is_test=0 and start_date is not null 
+	)	
+	,CTE AS
+	(
+	select contact_id,Start_Date,End_Date,
+	ROW_NUMBER() over(partition by contact_id order by Start_Date desc) AS RowNum
+	from distinctdate)
+   ,MembershipRenewalHistory AS
+   (
+	SELECT ConDim.Contact_SID
+		  ,CTE.Start_Date
+		  ,CTE.End_Date
+		  ,case when RowNum=1 then 1 else 0 end AS Is_Active
+	FROM CTE
+	JOIN dbo.Contact_Dim ConDim
+	  ON ConDIm.Contact_ID=CTE.contact_id)
+	MERGE dbo.Membership_Renewal_History AS target  
+    USING MembershipRenewalHistory AS source 
+    ON (target.Contact_SID = source.Contact_SID
+	   AND target.Start_Date=source.Start_Date
+	   AND target.End_Date=source.End_Date)  
+    WHEN MATCHED THEN
+        UPDATE SET Is_Active = source.Is_Active
+    WHEN NOT MATCHED THEN  
+        INSERT (Contact_SID,Start_Date,End_Date,Is_Active)  
+        VALUES (source.Contact_SID, source.Start_Date,source.End_Date,source.Is_Active);
+END
---
+++ EverGreenDw/StoredProcedure/dbo.Populate_Membership_Type_History.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROC [dbo].[Populate_Membership_Type_History]
+AS
+BEGIN
+	WITH distinctdate AS
+	(
+	select distinct contact_id,start_date as conversiondate,membership_type_id
+	from dbo.stage_civicrm_membership
+	where is_test=0 and start_date is not null
+	)	
+	,CTE AS
+	(
+	select contact_id,conversiondate,membership_type_id,
+	ROW_NUMBER() over(partition by contact_id order by conversiondate desc) AS RowNum
+	from distinctdate)
+   ,MembershipTypeHistory AS
+   (
+	SELECT ConDim.Contact_SID
+		  ,conversiondate AS Conversion_Date
+		  ,MemDim.Membership_Type AS Membership_Type
+		  ,case when RowNum=1 then 1 else 0 end AS Is_Current
+	FROM CTE
+	JOIN dbo.Contact_Dim ConDim
+	  ON ConDIm.Contact_ID=CTE.contact_id
+	JOIN dbo.Membership_Type_Dim MemDim
+	  ON MemDim.Membership_Type_ID=CTE.membership_type_id)
+	MERGE dbo.Membership_Type_History AS target  
+    USING MembershipTypeHistory AS source 
+    ON (target.Contact_SID = source.Contact_SID
+	   AND target.MemberShip_Type=source.MemberShip_Type
+	   AND target.Conversion_Date=source.Conversion_Date)  
+    WHEN MATCHED THEN
+        UPDATE SET Is_Current = source.Is_Current  
+    WHEN NOT MATCHED THEN  
+        INSERT (Contact_SID,Conversion_Date,Membership_Type,Is_Current)  
+        VALUES (source.Contact_SID, source.Conversion_Date,source.Membership_Type,source.Is_Current);
+END
